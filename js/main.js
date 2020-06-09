fb2bde9649bd8f31fcc326222d91e45b4592b8e6
GunnerJnr <gunnerjnr@live.co.uk>
2020-06-10 00:07:05 +0100

    SEO Cleanup Phase 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$(".header").height($(window).height());
 
-	$(".navbar a").click(function() {
+	$(".navbar a").click(function () {
 		$("body,html").animate(
-			{ scrollTop: $($.attr(this, "href")).offset().top-56 },
+			{ scrollTop: $($.attr(this, "href")).offset().top - 56 },
 			1000
 		);
 		return false;
